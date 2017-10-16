eecd121565201fd57d75a6481e012139536c5b43
riachan <32831543+riachan@users.noreply.github.com>
2017-10-16 14:55:51 +0700

    Add files via upload

---
+++ tugas-nurul.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="id">
+	<head>
+		<!-- Required meta tags -->
+		<title>Tugas Framework</title>
+		
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+		
+		<!-- CSS Background, -->
+		<style type="text/css">
+			.pinggiran{
+				border: 1px solid white;
+				color: black;
+			}
+			
+			.tinggi1{
+				height:210px;
+			}
+			
+			.tinggi2{
+				padding:10px;
+			}
+		</style>
+		
+	</head>
+	<body>
+		
+		<div class="container">
+			<div class="row justify-content-md-center pinggiran">
+				<div class="col col-lg-8 pinggiran" style="background: #00CC99; color: white">
+				  <h1><center/>TUGAS FRAMEWORK</h1>
+				</div>
+			</div>
+		  
+			<div class="row justify-content-md-center pinggiran">
+				<div class="col col-lg-3 pinggiran tinggi1" style="background: #00CCCC; color: white">
+					<div class="row justify-content-md-center">
+						<div class="col tinggi2">
+							<form action="index.html" method="post">
+							<button type="submit" name="kembali" class="btn btn-danger btn-block">Kembali</button>
+							</form>
+						</div>
+					</div>
+					<div class="row justify-content-md-center">
+						<div class="col tinggi2">
+							<center/><img src="chan.png" width="120" alt="image" class="rounded">
+						</div>
+					</div>
+				</div>
+				<div class="col col-lg-5 pinggiran" style="background: #00FFFF; color: black">
+					<b><h4><center/>GRID</h4></b>
+					<div class="row">
+						<div class="col col-3 tinggi2 pinggiran">
+							NPM
+						</div>
+						<div class="col tinggi2 pinggiran">
+							1155201913
+						</div>
+					</div>
+					<div class="row justify-content-md-center">
+						<div class="col col-3 tinggi2 pinggiran">
+							Nama
+						</div>
+						<div class="col tinggi2 pinggiran">
+							Nurul Qomariyah
+						</div>
+					</div>
+					<div class="row justify-content-md-center">
+						<div class="col col-3 tinggi2 pinggiran">
+							Matakuliah
+						</div>
+						<div class="col tinggi2 pinggiran">
+							Pemograman Framework
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row justify-content-md-center pinggiran">
+				<div class="col col-lg-8 pinggiran" style="background: #00CC99; color: white">
+					<center/>Tugas Framework
+				</div>
+			</div>
+		</div>
+
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+	</body>
+</html>
